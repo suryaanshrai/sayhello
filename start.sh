4f253275e2389f44825edd589eb52481bb786032
suryaanshrai <102371942+suryaanshrai@users.noreply.github.com>
2024-03-17 18:18:26 +0530

    Update

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/suryaanshrai/sayhello.git
+git pull
 pip install -r requirements.txt
 sudo kill `sudo lsof -t -i :80` 
 sudo python3 manage.py runserver 80 > logs&
