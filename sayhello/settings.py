0bf12a4805486ace7c9231cca86dc368d3170e98
suryaanshrai <102371942+suryaanshrai@users.noreply.github.com>
2024-03-17 18:38:28 +0530

    Test2

--- sayhello/settings.py
+++ sayhello/settings.py
@@ -102,10 +102,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
