ab75594390faf85b62eadc87c02429d36c8348f3
suryaanshrai <102371942+suryaanshrai@users.noreply.github.com>
2024-03-17 18:34:30 +0530

    Update

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 git pull
 pip install -r requirements.txt
 sudo kill `sudo lsof -t -i :80` 
-sudo python3 manage.py runserver 80 > logs&
+sudo nohup python3 manage.py runserver 127.0.0.1:80 > server.log 2>&1 &
